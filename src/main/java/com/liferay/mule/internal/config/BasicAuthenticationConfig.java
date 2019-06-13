408d582f5d7a0259fbeae06622f155f7abe4ba7a
Matija Petanjek <matija.petanjek@liferay.com>
2019-06-13 17:40:40 +0200

    COMMERCE-1151 - mulesoft - add config classes for Basic and OAuth2 authentication (only basic is impemented for now)

---
+++ src/main/java/com/liferay/mule/internal/config/BasicAuthenticationConfig.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mule.internal.config;
+
+import org.mule.runtime.api.meta.ExpressionSupport;
+import org.mule.runtime.extension.api.annotation.Expression;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.annotation.param.display.Password;
+import org.mule.runtime.extension.api.annotation.param.display.Placement;
+
+/**
+ * @author Matija Petanjek
+ */
+public class BasicAuthenticationConfig {
+
+	public String getPassword() {
+		return _password;
+	}
+
+	public String getUsername() {
+		return _username;
+	}
+
+	public void setPassword(String password) {
+		_password = password;
+	}
+
+	public void setUsername(String username) {
+		_username = username;
+	}
+
+	@DisplayName("Password")
+	@Parameter
+	@Password
+	@Placement(order = 2)
+	private String _password;
+
+	@DisplayName("Username")
+	@Expression(ExpressionSupport.NOT_SUPPORTED)
+	@Parameter
+	@Placement(order = 1)
+	private String _username;
+
+}
---
+++ src/main/java/com/liferay/mule/internal/config/OAuth2AuthenticationConfig.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mule.internal.config;
+
+import org.mule.runtime.api.meta.ExpressionSupport;
+import org.mule.runtime.extension.api.annotation.Expression;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.Placement;
+
+/**
+ * @author Matija Petanjek
+ */
+public class OAuth2AuthenticationConfig {
+
+	public String getConsumerKey() {
+		return _consumerKey;
+	}
+
+	public String getConsumerSecret() {
+		return _consumerSecret;
+	}
+
+	public void setConsumerKey(String consumerKey) {
+		_consumerKey = consumerKey;
+	}
+
+	public void setConsumerSecret(String consumerSecret) {
+		_consumerSecret = consumerSecret;
+	}
+
+	@Expression(ExpressionSupport.NOT_SUPPORTED)
+	@Parameter
+	@Placement(order = 1)
+	private String _consumerKey;
+
+	@Expression(ExpressionSupport.NOT_SUPPORTED)
+	@Parameter
+	@Placement(order = 2)
+	private String _consumerSecret;
+
+}
