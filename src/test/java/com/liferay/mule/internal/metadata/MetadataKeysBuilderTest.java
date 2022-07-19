liferay-core-infra/liferay-portal

LPS-153088 Migrate Mockito API to latest (Misc)

modules/etl/mulesoft/src/test/java/com/liferay/mule/internal/metadata/MetadataKeysBuilderTest.java
@@ -29,7 +29,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import org.mule.runtime.api.connection.ConnectionException;
@@ -62,7 +61,7 @@ public class MetadataKeysBuilderTest {
 		).when(
 			metadataKeysBuilder
 		).getOASJsonNode(
-			Matchers.anyObject()
+			Mockito.anyObject()
 		);
 	}
 

liferay/liferay-etl-mulesoft

src/test/java/com/liferay/mule/internal/metadata/MetadataKeysBuilderTest.java
@@ -29,7 +29,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import org.mule.runtime.api.connection.ConnectionException;
@@ -62,7 +61,7 @@ public class MetadataKeysBuilderTest {
 		).when(
 			metadataKeysBuilder
 		).getOASJsonNode(
-			Matchers.anyObject()
+			Mockito.any()
 		);
 	}
 